ca80a3bbaf8ccd84a385bbbfa162327257934312
Patch95 <patch2995@gmail.com>
2018-01-26 13:47:24 -0600

    Reasearch Logger instalador

---
+++ ResearchLoggerInstaler.bat
@@ -0,0 +1,63 @@
+@echo off
+echo Welcome to ResearchLogger Instalation
+
+echo Installing dependencies
+
+echo Installing python
+start .\python-2.7.14.msi
+echo If Python installation is finished
+pause
+
+echo Installing pygtk
+start pygtk-all-in-one-2.24.0.win32-py2.7.msi
+echo If pygtk installation is finished
+pause
+
+echo Installing PIL
+start PIL-1.1.7.win32-py2.7.exe
+echo If PIL installation is finished
+pause
+
+echo Installing pyHook
+start pyHook-1.5.1.win32-py2.7
+echo If pyHook installation is finished
+pause
+
+echo Installing pywin32
+start pywin32-219.win32-py2.7.exe
+echo If pywing installation is finished
+pause
+
+echo Installing configobjg
+start configobj-4.7.2\setup.py
+echo If configobj installation is finished
+pause
+
+echo Dependencies installed
+
+echo Copying files ...
+set actualPath=%cd%
+set HeadPath=%actualPath:~0,3%
+
+move ResearchLogger %HeadPath%
+
+echo ReasearchLogger located in:
+echo.%HeadPath%
+
+pause
+
+set "python=Python27\"
+set "pythonPath=%HeadPath%%python%"
+
+set "libOrigin=Lib\site-packages\pywin32_system32\pywintypes27.dll"
+set "libDestination=Lib\site-packages\win32\"
+
+set "pathOrigin=%pythonPath%%libOrigin%"
+set "pathDestination=%pythonPath%%libDestination%"
+
+copy  %pathOrigin% %pathDestination%
+
+echo Files copied, installation completed!!
+
+pause
+exit
